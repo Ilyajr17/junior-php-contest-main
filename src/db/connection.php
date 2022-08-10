37ef36d027a96384a03f72b565a26fd6d2da67b2
Ilya Zherebtsov <izherebcov@gmail.com>
2022-08-10 10:42:34 +0400

    Create connection to Sqlite DB

--- src/db/connection.php
+++ src/db/connection.php
@@ -2,14 +2,25 @@
 
 namespace App\db\connection;
 
+use PDO;
+
 function createConnection()
 {
     $dbPath = __DIR__ . '/../../db.sqlite';
-    touch($dbPath);
 
     $db = null;
+
+    $db = new PDO('sqlite:' . $dbPath);
 
     //TODO: Create connection to Sqlite DB
 
     return $db;
 }
+
+//$pdo = createConnection();
+
+
+print_r(createConnection()->connect());
+
+
+
